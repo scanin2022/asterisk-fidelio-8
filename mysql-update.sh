73f01ec71c9421cbcdb18b6275726bad8af94eb4
Anton <116804557+scanin2022@users.noreply.github.com>
2022-10-29 11:28:45 +0300

    Update mysql-update.sh

--- mysql-update.sh
+++ mysql-update.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-mysql --batch --silent -ufreepbxuser -p9bddf9b6fcfa -Dasterisk <<<"UPDATE asterisk.billing_extensions SET permission_id=$2 WHERE billing_extensions.sip_num =$1 LIMIT 1;"
+mysql --batch --silent -ufreepbxuser -yourpassword -Dasterisk <<<"UPDATE asterisk.billing_extensions SET permission_id=$2 WHERE billing_extensions.sip_num =$1 LIMIT 1;"
